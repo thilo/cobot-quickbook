e062f8657243c2c67b3771c545c2c401f7d95109
Thilo Utke <thilo@upstre.am>
2013-06-17 21:50:59 +0200

    storing the token with the user. ouch!

--- app/controllers/session.rb
+++ app/controllers/session.rb
@@ -2,7 +2,7 @@ CobotFb.controller do
   get '/session/new' do
     if oauth_session
       user_info = JSON.parse(oauth_session.get("http://www.cobot.me/api/user").body)
-      self.current_user = User.find_or_create_for_cobot_id(user_info['id'])
+      self.current_user = User.find_or_create_for_cobot_id(user_info['id'], {token: session[:token]})
       redirect '/user'
     else
       redirect '/cobot/auth'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ActiveRecord::Base
-  validates_presence_of :cobot_id
+  validates_presence_of :cobot_id, :token
   has_many :spaces
 
-  def self.find_or_create_for_cobot_id(cobot_id)
+  def self.find_or_create_for_cobot_id(cobot_id, attributes = {})
     user = User.find_by_cobot_id(cobot_id)
-    user || User.create!(cobot_id: cobot_id)
+    user || User.create!(attributes.merge({cobot_id: cobot_id}))
   end
   
   def qb_connected?
