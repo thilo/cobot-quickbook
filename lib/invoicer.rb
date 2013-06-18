95d45b2cdb715dd02a26e9b0165710025ae4204e
Thilo Utke <thilo@upstre.am>
2013-06-18 15:18:24 +0200

    wrong method name.

--- lib/invoicer.rb
+++ lib/invoicer.rb
@@ -11,6 +11,6 @@ class Invoicer
     qb_oauth_client = qb_oauth_client(user)
     oauth_session = get_oauth_session(user.token)
     exporter = InvoiceExporter.new(space, oauth_session, qb_oauth_client)
-    exporter.run(Date.today.beginning_of_month, Date.today.end_of_month)
+    exporter.export(Date.today.beginning_of_month, Date.today.end_of_month)
   end
 end
