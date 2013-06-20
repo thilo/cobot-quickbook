776af7f4e0673101623f871101ffdfc015b6a720
Thilo Utke <thilo@upstre.am>
2013-06-20 13:47:08 +0200

    changed run_once, to accept start end end date optionally to import more than just today invoices.

--- lib/invoicer.rb
+++ lib/invoicer.rb
@@ -6,11 +6,11 @@ class Invoicer
     end
   end
   
-  def run_one(space)
+  def run_one(space, start_date = Date.yesterday, end_date = Date.today)
     user =  space.user
     qb_oauth_client = qb_oauth_client(user)
     oauth_session = get_oauth_session(user.token)
     exporter = InvoiceExporter.new(space, oauth_session, qb_oauth_client)
-    exporter.export(Date.today.beginning_of_month, Date.today.end_of_month)
+    exporter.export(start_date, end_date)
   end
 end
