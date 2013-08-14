f4090ea2384b1773f0ee561b64805ecdc01e6063
Thilo Utke <thilo@upstre.am>
2013-08-14 15:55:31 +0200

    show 100 income accounts

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -5,7 +5,7 @@ CobotFb.controller do
     user_info = JSON.parse(oauth_session.get("http://www.cobot.me/api/user").body)
     @spaces_json = user_info['admin_of'].to_json
     if current_user.qb_connected?
-      @qb_accounts = Quickeebooks::Online::Service::Account.new(qb_oauth_client(current_user), current_user.qb_realm_id).list([], 1, 50).entries
+      @qb_accounts = Quickeebooks::Online::Service::Account.new(qb_oauth_client(current_user), current_user.qb_realm_id).list([], 1, 100).entries
     end
     @enabled_spaces_ids = current_user.spaces.map(&:cobot_id).to_json
     render "user/show"
