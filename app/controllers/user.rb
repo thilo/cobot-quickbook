80dd9e917b9a4f2b0a685e9a84364ac07754ce85
Thilo Utke <thilo@upstre.am>
2013-08-14 16:31:56 +0200

    fixed qb accounts list

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -7,9 +7,9 @@ CobotFb.controller do
     if current_user.qb_connected?
       @qb_accounts = []
       page = 1
-      while (qb_accounts = Quickeebooks::Online::Service::Account.new(qb_oauth_client(current_user), current_user.qb_realm_id).list([], page, 50).size).length > 0 do
-        @qb_accounts << qb_accounts
-        page = page + 1
+      while (qb_accounts = Quickeebooks::Online::Service::Account.new(qb_oauth_client(current_user), current_user.qb_realm_id).list([], page, 50).entries).count > 0 do
+        @qb_accounts += qb_accounts
+        page += 1
       end
     end
     @enabled_spaces_ids = current_user.spaces.map(&:cobot_id).to_json
