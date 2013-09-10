cd1a643ccf7c1c68d2ac8fb8136b421ca991162c
Alexander Lang <alex@upstre.am>
2013-09-10 11:40:25 +0200

    ignores rejected tokens

--- lib/invoicer.rb
+++ lib/invoicer.rb
@@ -4,12 +4,14 @@ class Invoicer
     Space.all.each do |space|
       begin
         Invoicer.new.run_one(space)
+      rescue OAuth::Problem => e
+        CobotFb.logger.info "Error in invoicer for space #{space.name}: #{e.message}"
       rescue Exception => exception
          Raven.capture_exception(exception, extra: {space: space.to_s})
       end
     end
   end
-  
+
   def run_one(space, start_date = 8.days.ago.to_date, end_date = 7.days.ago.to_date)
     user =  space.user
     qb_oauth_client = qb_oauth_client(user)
