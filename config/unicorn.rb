80a53963e38089f5eb9f6fc1e64c19d2352387da
Thilo Utke <thilo@upstre.am>
2013-10-09 16:37:15 +0200

    improved unicorn config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,2 +1,23 @@
-worker_processes 3 # amount of unicorn workers to spin up
-timeout 10         # restarts workers that hang for 10 seconds
+worker_processes 3
+timeout 10
+preload_app true
+
+before_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
+end 
+
+after_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+  end
+
+  defined?(ActiveRecord::Base) and
+    config = ActiveRecord::Base.configurations[ENV['RACK_ENV'].to_sym]
+    ActiveRecord::Base.establish_connection(config)
+end
